e4831f15b156aca1ebdc9c89b147b7b3744bae48
Lucas Saito <lucasesaito@hotmail.com>
2021-02-24 15:42:46 -0300

    Initial Commit

---
+++ MenuBarShortcut/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MenuBarShortcut
+//
+//  Created by Lucas Eiji Saito on 24/02/21.
+//  Copyright © 2021 Lucas Eiji Saito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
