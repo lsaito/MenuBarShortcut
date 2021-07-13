e550242a57ea613a28ae72ddf3527ca07c568480
Estaife Lima <estaife@icloud.com>
2021-07-12 22:54:36 -0300

    Added setup.sh to create key in keychain and created Readme to help configure setup.sh

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Setup
+###### Open file setup.sh, change your_password and your_secret, after run this commands below:
+
+```console
+$ chmod +x ./setup.sh
+$ ./setup.sh 
+```
---
+++ setup.sh
@@ -0,0 +1,2 @@
+security add-generic-password -s 'MenuBarShortcut_PASSWORD'  -a '${whoami}' -w 'your_password'
+security add-generic-password -s 'MenuBarShortcut_SECRET'  -a '${whoami}' -w 'your_secret'
